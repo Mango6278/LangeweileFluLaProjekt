58ed1fb69e8a56e9bae65451e563cbf6ba4775e7
Mango6278 <mika.laubert@gmx.de>
2025-08-08 12:48:45 +0200

    fskjdldjf

--- main/Flarm.cpp
+++ main/Flarm.cpp
@@ -508,7 +508,7 @@ void Flarm::drawFlarmWarning(){
 	e_audio_alarm_type_t alarm = AUDIO_ALARM_FLARM_1;
 	if( AlarmLevel == 3 ) { // highest, impact 0-8 seconds
 		volume = flarm_volume.get();
-		alarm = AUDIO_ALARM_FLARM_OFF;
+		alarm = AUDIO_ALARM_OFF;
 		Sound::playSound(HI, true);
 	}
 	else if( AlarmLevel == 2 ){
